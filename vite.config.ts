d27fe4ffe57eefaad3e8532d40e53f0002b62735
nextwaveaisuite <nextwaveaisuite@gmail.com>
2026-01-31 16:17:33 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 
-const __filename = fileURLToPath(import.meta.url);
+const __filename = fileURLToPath(import.meta.url );
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
