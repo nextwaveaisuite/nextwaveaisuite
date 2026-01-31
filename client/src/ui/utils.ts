40e9ad4d15bfb4d3d888a55896bfc6833e8995cf
nextwaveaisuite <nextwaveaisuite@gmail.com>
2026-01-31 17:56:31 +1000

    Update utils.ts

--- client/src/ui/utils.ts
+++ client/src/ui/utils.ts
@@ -1,1 +1,10 @@
-utils.ts
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+/**
+ * Tailwind className merge helper (shadcn-style).
+ * Safe even if you use regular class strings.
+ */
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
